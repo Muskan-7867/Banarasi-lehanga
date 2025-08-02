5d1bf5b7f2e4719cfa56f0d0b04eb34bd423bade
Muskan-7867 <muskanloach984@gmail.com>
2025-08-02 17:15:09 +0530

    add arrows

--- src/components/user/home/components/Banner.tsx
+++ src/components/user/home/components/Banner.tsx
@@ -1,17 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
 export default function Banner() {
   return (
     <div className="w-full aspect-[16/6] relative mt-6">
       <Image
-        src="https://kalki.gumlet.io/cdn/shop/files/2_slider_banner_banner_new_arrival_all_country_1640x615_1_8_25.jpg?w=1480&"
+        src="https://www.cbazaar.com/blog/wp-content/uploads/2020/01/WP_BLog_Jan21.jpg"
         alt="banner"
         fill
         className="object-cover"
         priority
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 1400px"
       />
+      <div className="absolute top-1/2 left-4 -translate-y-1/2 ">
+        <SlArrowLeft size={28}/>
+      </div>
+
+      <div className="absolute top-1/2 right-4 -translate-y-1/2 ">
+        <SlArrowRight size={28}/>
+      </div>
+      
     </div>
-  )
+  );
 }
