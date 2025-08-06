d2905e7b0964c2af7b18ba3b4d56e6f470d074de
Muskan-7867 <muskanloach984@gmail.com>
2025-08-06 18:27:29 +0530

    update

--- src/app/(user)/(home)/page.tsx
+++ src/app/(user)/(home)/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import Header from "../../../components/user/home/components/Header";
-import Navbar from "@/components/user/home/components/Navbar";
-import SecondNavbar from "@/components/user/home/components/SecondNavbar";
 import Banner from "@/components/user/home/components/Banner";
 import Features from "@/components/user/home/components/Features";
 import Products from "@/components/user/home/components/Products";
 import SwiperCards from "@/components/user/home/components/SwiperCards";
-
 import StyleProducts from "@/components/user/home/components/StyleProducts";
 import SecondBanner from "@/components/user/home/components/SecondBanner";
 import ImageSections from "@/components/user/home/components/ImageSections";
@@ -14,19 +10,16 @@ import ImageSections from "@/components/user/home/components/ImageSections";
 export default function Homepage() {
   return (
     <div className=" w-full min-h-screen flex flex-col">
-      <Header />
-      <Navbar />
-      <SecondNavbar />
       <Banner />
       <Features />
       <Products />
       <SwiperCards />
-     <StyleProducts />
-     <Products />
-     <SecondBanner />
- 
-     <StyleProducts />
-     <ImageSections />
+      <StyleProducts />
+      <Products />
+      <SecondBanner />
+      <StyleProducts />
+      <ImageSections />
+      <StyleProducts />
     </div>
   );
 }
