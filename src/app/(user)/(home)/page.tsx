bfb26c5f5d09f23943dcdd95a783b1d4f73b5a93
Muskan-7867 <muskanloach984@gmail.com>
2025-08-07 15:12:56 +0530

    add components

--- src/app/(user)/(home)/page.tsx
+++ src/app/(user)/(home)/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Banner from "@/components/user/home/components/Banner";
+import Banner from "@/components/user/banners/Banner";
 import Features from "@/components/user/home/components/Features";
 import Products from "@/components/user/home/components/Products";
 import SwiperCards from "@/components/user/home/components/SwiperCards";
 import StyleProducts from "@/components/user/home/components/StyleProducts";
-import SecondBanner from "@/components/user/home/components/SecondBanner";
+import SecondBanner from "@/components/user/banners/SecondBanner";
 import ImageSections from "@/components/user/home/components/ImageSections";
+import GridBanner from "@/components/user/home/components/GridBanner";
+import BridalBaneer from "@/components/user/banners/BridalBaneer";
 
 export default function Homepage() {
   return (
@@ -20,6 +22,10 @@ export default function Homepage() {
       <StyleProducts />
       <ImageSections />
       <StyleProducts />
+      <GridBanner />
+      <Products  />
+      <BridalBaneer />
+      <Products />
     </div>
   );
 }
