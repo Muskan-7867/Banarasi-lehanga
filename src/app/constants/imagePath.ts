0e6cef01cfa0c6e95d7a993be6bd7654ecef1ad9
Muskan-7867 <muskanloach984@gmail.com>
2025-08-07 15:12:34 +0530

    add images

--- src/app/constants/imagePath.ts
+++ src/app/constants/imagePath.ts
@@ -3,9 +3,22 @@ import delivery from "../../../public/images/delivery-truck.png"
 import quality from "../../../public/images/quality.png"
 import support from "../../../public/images/customer-service.png"
 
+import saree from "../../../public/images/saree.png"
+import saree1 from "../../../public/images/saree1.png"
+import lehanga from "../../../public/images/lehanga.png"
+import suit from "../../../public/images/suit.png"
+import indo from "../../../public/images/indo.png"
+
 export {
     EasyReturn,
     delivery,
     quality,
-    support
+    support,
+    saree,
+    saree1,
+    lehanga,
+    suit,
+    indo
+
+
 }
