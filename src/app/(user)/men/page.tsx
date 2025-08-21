a18cd372b07bf916299570485d24240410ed4661
Muskan-7867 <muskanloach984@gmail.com>
2025-08-21 12:03:50 +0530

    update

--- src/app/(user)/men/page.tsx
+++ src/app/(user)/men/page.tsx
@@ -1,25 +1,22 @@
-import CategorySection from '@/components/user/men/components/CategorySection'
-import MenBanner from '@/components/user/men/banners/MenBanner'
-import MenProducts from '@/components/user/men/components/MenProducts'
-import React from 'react'
-import MenBanner2 from '@/components/user/men/banners/MenBanner2'
-import CollectionSection from '@/components/user/men/components/CollectionSection'
-import MenDescription from '@/components/user/men/components/MenDescription'
-import VideoCardSection from '@/components/user/men/components/VideoCardSection'
+import CategorySection from "@/components/user/men/components/CategorySection";
+import MenBanner from "@/components/user/men/banners/MenBanner";
+import MenProducts from "@/components/user/men/components/MenProducts";
+import React from "react";
+import MenBanner2 from "@/components/user/men/banners/MenBanner2";
+import CollectionSection from "@/components/user/men/components/CollectionSection";
+import MenDescription from "@/components/user/men/components/MenDescription";
+import VideoCardSection from "@/components/user/men/components/VideoCardSection";
 
 export default function MenPage() {
   return (
-   <>
-     <MenProducts />
-     <MenBanner />
-     <CategorySection />
-     <MenBanner2 />
-     <CollectionSection />
-     <VideoCardSection />
-     <MenDescription />
-  
- 
-
-   </>
-  )
+    <>
+      <MenProducts />
+      <MenBanner />
+      <CategorySection />
+      <MenBanner2 />
+      <CollectionSection />
+      <VideoCardSection />
+      <MenDescription />
+    </>
+  );
 }
