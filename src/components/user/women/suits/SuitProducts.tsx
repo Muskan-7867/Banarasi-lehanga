381ca5823d843023086fd0776030df08d78ed70a
Muskan-7867 <muskanloach984@gmail.com>
2025-08-21 12:02:43 +0530

    add products

--- src/components/user/women/suits/SuitProducts.tsx
+++ src/components/user/women/suits/SuitProducts.tsx
@@ -1,16 +1,17 @@
 "use client";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import FilterBar from "./FilterBar";
+
 import ProductCard from "@/components/ui/ProductCard";
 import TitleWrapper from "@/components/wrappers/productcard/TitleWrapper";
 import SectionHeading from "@/components/ui/Category/CategorySectionHeading";
 import { getProductsByTagQuery } from "@/lib/query";
 import { ProductT } from "@/types";
 import Link from "next/link";
+import FilterBar from "../bridallehanga/components/FilterBar";
 
-export default function ProductsSection() {
-  const tag = "Bridal Lehanga";
+export default function SuitProducts() {
+  const tag = "Suits";
 
   const { data, isLoading, isError } = useQuery(getProductsByTagQuery(tag));
 
@@ -21,7 +22,7 @@ export default function ProductsSection() {
   return (
     <div className="flex-1 text-black">
       <SectionHeading>Indian Ethnic Wear Bestsellers</SectionHeading>
-      <FilterBar />
+      <FilterBar/>
       <div className="mt-6 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 px-2 md:px-0">
         {data?.map((product: ProductT) => (
             <Link key={product.id} href={`/products/${product.id}`}>
