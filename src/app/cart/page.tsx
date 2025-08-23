49b0b6a1ce225342b4f34d43b686242879f8a224
Muskan-7867 <muskanloach984@gmail.com>
2025-08-23 16:19:00 +0530

    add cart

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,10 @@
+import CartPage from "@/components/user/cart/CartPage";
+import React from "react";
+
+export default function MainCartPage() {
+  return (
+    <>
+      <CartPage />
+    </>
+  );
+}
