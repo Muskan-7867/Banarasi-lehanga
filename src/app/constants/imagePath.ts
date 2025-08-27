f8f1932abb39f265b02e2dbc03e25792944e25be
Muskan-7867 <muskanloach984@gmail.com>
2025-08-27 17:13:51 +0530

    update

--- src/app/constants/imagePath.ts
+++ src/app/constants/imagePath.ts
@@ -10,16 +10,12 @@ import suit from "../../../public/images/suit.png"
 import indo from "../../../public/images/indo.png"
 
 import login from "../../../public/images/login.jpg"
-import post1 from "../../../public/images/post1.jpg"
-import emptypost from "../../../public/images/emptypost.jpg"
-import post2 from "../../../public/images/post2.jpg"
-import post3 from "../../../public/images/post3.jpg"
-import post4 from "../../../public/images/post4.jpg"
-import instapost1 from "../../../public/images/instapost1.jpg"
-import instapost2 from "../../../public/images/instapost2.jpg"
+
+
+
+
 import instapost3 from "../../../public/images/instapost3.jpg"
-import instapost4 from "../../../public/images/instapost4.jpg"
-import instapost5 from "../../../public/images/instapost5.jpg"
+
 import menbanner1 from "../../../public/images/banner1.jpg"
 import menbanner2 from "../../../public/images/banner2.avif"
 import homebanner1 from "../../../public/images/homebanner3.jpg"
@@ -45,16 +41,10 @@ export {
     indo,
  
     login,
-    post1,
-    emptypost,
-    post2,
-    post3,
-    post4,
-    instapost1,
-    instapost2,
+
+  
     instapost3,
-    instapost4,
-    instapost5,
+  
     menbanner1,
     menbanner2,
     homebanner1,
